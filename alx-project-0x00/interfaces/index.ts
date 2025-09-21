f56624dcbcaabf93a176fcce37bf4126f395f5b7
codebyemma <mysubbacc@gmail.com>
2025-09-21 22:31:03 +0100

    gf

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,14 @@
 export interface PillProps {
   title: string
 }
+
+// interfaces/index.tsx
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  className?: string;
+  children?: ReactNode;
+}
